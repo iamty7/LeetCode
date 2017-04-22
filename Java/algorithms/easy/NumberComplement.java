15830ca6bb6a55b6e7a1ca06c5c51522eb0c7f62
iamty <taoyu1994x@gmail.com>
2017-04-22 13:06:45 +0000

    New Problem Solution - Number Complement

---
+++ Java/algorithms/easy/NumberComplement.java
@@ -0,0 +1,31 @@
+package easy;
+
+public class NumberComplement {
+	/*
+	 * Given a positive integer, output its complement number. The complement
+	 * strategy is to flip the bits of its binary representation.
+	 * 
+	 * Note: The given integer is guaranteed to fit within the range of a 32-bit
+	 * signed integer. You could assume no leading zero bit in the integer’s
+	 * binary representation. Example 1: Input: 5 Output: 2 Explanation: The
+	 * binary representation of 5 is 101 (no leading zero bits), and its
+	 * complement is 010. So you need to output 2. Example 2: Input: 1 Output: 0
+	 * Explanation: The binary representation of 1 is 1 (no leading zero bits),
+	 * and its complement is 0. So you need to output 0.
+	 */
+
+	public int findComplement(int num) {
+		// Your runtime beats 89.63% of java submissions. Runtime 10ms
+		int highBit = 0;
+		for (int i = 31; i >= 0; i--) {
+			if ((num >>> i) == 1) {
+				highBit = i;
+				break;
+			}
+		}
+		for (int i = highBit; i >= 0; i--) {
+			num ^= 1 << i;
+		}
+		return num;
+	}
+}
