9dd00f9df9d9a74a07999103fa94ab53e048b64d
iamty <taoyu1994x@gmail.com>
2017-04-22 10:38:04 +0000

    New Problem Solution - Number of 1 Bits

---
+++ Java/algorithms/easy/NumberOf1Bits.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class NumberOf1Bits {
+	/*
+	 * Write a function that takes an unsigned integer and returns the number of
+	 * ’1' bits it has (also known as the Hamming weight).
+	 * 
+	 * For example, the 32-bit integer ’11' has binary representation
+	 * 00000000000000000000000000001011, so the function should return 3.
+	 */
+	public int hammingWeight(int n) {
+		// Your runtime beats 91.46% of java submissions. Runtime 1ms
+		int cnt = 0;
+		while (n != 0) {
+			n = n & (n - 1);
+			cnt++;
+		}
+		return cnt;
+	}
+}
