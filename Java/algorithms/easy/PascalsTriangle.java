3a00153b7f22db8653d998789a6a7f9bd641dfdb
iamty <taoyu1994x@gmail.com>
2017-09-04 18:12:10 -0400

    Add Solution to Pascal's Triangle

--- Java/algorithms/easy/PascalsTriangle.java
+++ Java/algorithms/easy/PascalsTriangle.java
@@ -29,4 +29,22 @@ public class PascalsTriangle {
 
 		return tri;
 	}
+
+	public List<List<Integer>> solution2(int numRows) {
+		// Your runtime beats 19.33 % of java submissions. Runtime 1ms
+		// !!Use a row to calculate row by row.
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> row, pre = null;
+		for (int i = 0; i < numRows; ++i) {
+			row = new ArrayList<Integer>();
+			for (int j = 0; j <= i; ++j)
+				if (j == 0 || j == i)
+					row.add(1);
+				else
+					row.add(pre.get(j - 1) + pre.get(j));
+			pre = row;
+			res.add(row);
+		}
+		return res;
+	}
 }
