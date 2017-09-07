3cc07027728479cd287b34d988931e47ec202ca2
iamty <taoyu1994x@gmail.com>
2017-09-07 00:26:06 -0400

    Add Solutions to Remove Duplicates from Sorted Array

--- Java/algorithms/easy/RemoveDuplicatesfromSortedArray.java
+++ Java/algorithms/easy/RemoveDuplicatesfromSortedArray.java
@@ -2,6 +2,20 @@ package easy;
 
 public class RemoveDuplicatesfromSortedArray {
 
+	/*
+	 * Given a sorted array, remove the duplicates in place such that each
+	 * element appear only once and return the new length.
+	 * 
+	 * Do not allocate extra space for another array, you must do this in place
+	 * with constant memory.
+	 * 
+	 * For example, Given input array nums = [1,1,2],
+	 * 
+	 * Your function should return length = 2, with the first two elements of
+	 * nums being 1 and 2 respectively. It doesn't matter what you leave beyond
+	 * the new length.
+	 */
+
 	public int solution(int[] nums) {
 
 		// �Ƿ��������������ͬ�����ֱ�i++�����������
@@ -50,4 +64,14 @@ public class RemoveDuplicatesfromSortedArray {
 		return ++j;
 	}
 
+	public int solution4(int[] nums) {
+		// Your runtime beats 89.01 % of java submissions. Runtime 12ms
+		int i = 0;
+		for (int n : nums) {
+			if (n != nums[i])
+				i++;
+			nums[i] = n;
+		}
+		return i + 1;
+	}
 }
